da9e2f532d50878067d61143592de23bdfb565dd
Amirg03 <115162882+Amirg03@users.noreply.github.com>
2024-11-21 10:59:15 +0330

    [add] AStar Search

--- main.py
+++ main.py
@@ -25,23 +25,23 @@ class BFS_Agent(AbstractSearchAgent):
         while(not (reachedToGoal)):
             current_state = self.OPEN[0]
             neighbors = self.get_neighbor(current_state)
-
-            #checking if goal is in our neighbors or not
+            
             for i in neighbors:
+
+                #checking if goal is in our neighbors or not
                 if(i==s_goal):
                     self.PARENT[i] = current_state
                     reachedToGoal = True
+
+                #Add unvisited neighbors to open list
+                if(not (i in self.CLOSED) and not (i in self.OPEN)):
+                    self.OPEN.append(i)
+                    #if(not (i in self.PARENT)):
+                    self.PARENT[i] = current_state
+                    visited.append(i)
+
             if(reachedToGoal):
                 break
-            
-            #Add unvisited neighbors to open list
-            
-            for j in neighbors:
-                if(not (j in self.CLOSED) and not (j in self.OPEN)):
-                    self.OPEN.append(j)
-                    #if(not (j in self.PARENT)):
-                    self.PARENT[j] = current_state
-                    visited.append(j)
 
             self.CLOSED.append(current_state)
             self.OPEN.remove(current_state)
@@ -71,17 +71,11 @@ class DFS_Agent(AbstractSearchAgent):
         visited = []
         reachedToGoal = False
         self.OPEN.append(s_start)
-        count = 0
 
         while(not (reachedToGoal)):
             current_state = self.OPEN.pop()
             self.CLOSED.append(current_state)
             visited.append(current_state)
-
-            """if(current_state==s_goal):
-                reachedToGoal = True
-                break
-            """
 
             neighbors = self.get_neighbor(current_state)
             for i in neighbors:
@@ -94,7 +88,9 @@ class DFS_Agent(AbstractSearchAgent):
                     self.PARENT[i] = current_state
 
             if(reachedToGoal):
+                print(self.PARENT)
                 break
+
             
 
 
@@ -122,7 +118,43 @@ class AStar_Agent(AbstractSearchAgent):
         * visted (list): list of visited nodes
         """
 
-        # TODO
+        visited = []
+        realCosts = dict()
+        self.OPEN.append(s_start)
+        realCosts[s_start] = 0
+
+
+        while(True):
+            minCost = 10000
+            #Choosing best option from open list
+            for i in self.OPEN:
+                if((realCosts[i] + max(abs(s_goal[0]-i[0]),abs(s_goal[1]-i[1]))) < minCost):
+                    minCost = realCosts[i] + max(abs(s_goal[0]-i[0]),abs(s_goal[1]-i[1]))
+                    current_state = i
+            
+            visited.append(current_state)
+                    
+            if(current_state == s_goal):
+                break
+                    
+            neighbors = self.get_neighbor(current_state)
+            
+            for j in neighbors:
+                if(not (j in self.CLOSED) and not (j in self.OPEN)):
+                    self.OPEN.append(j)
+                    if(not (j in self.PARENT)):
+                        self.PARENT[j] = current_state
+                        realCosts[j] = realCosts[current_state]+1
+            
+            self.OPEN.remove(current_state)
+            self.CLOSED.append(current_state)
+
+                        
+        return self.extract_path(self.PARENT),visited
+                    
+                
+
+
 
 if __name__ == "__main__":
     s_start = (5, 5) # Starting point
@@ -136,7 +168,7 @@ if __name__ == "__main__":
         gn.main(map_name)
     
     else:
-        agent = BFS_Agent(s_start, s_goal, map_name) # Choose the agent here
+        agent = AStar_Agent(s_start, s_goal, map_name) # Choose the agent here
         path, visited = agent.searching()
 
         # Plotting the path
